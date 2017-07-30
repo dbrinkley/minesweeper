843966bf6055e620c9ac2a3aba3371ceb547c75d
dbrinkley <personal@dylanbrinkley.com>
2017-07-30 16:40:03 -0700

    Version 4.0

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -3,16 +3,20 @@ class Game {
     this._board = new Board(numberOfRows,numberOfColumns,numberOfBombs);
   }
 
+  get board() {
+    return this._board;
+  }
+
   playMove(rowIndex,columnIndex) {
     this._board.flipTile(rowIndex,columnIndex);
-    if(this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('You have lost the gaeme');
+    if(this._board._playerBoard[rowIndex][columnIndex] === 'B') {
+      console.log('You have lost the game');
       this._board.print();
     } else if (this._board.hasSafeTiles()) {
       console.log('You Won!!!!');
       this._board.print();
     } else {
-      consloe.log('Current Board:');
+      console.log('Current Board:');
       this._board.print();
     }
   }
@@ -25,25 +29,39 @@ class Board {
     this._numberOfTiles = numberOfRows * numberOfColumns;
     this._playerBoard = Board.generatePlayerBoard(numberOfRows,numberOfColumns);
     this._bombBoard = Board.generateBombBoard(numberOfRows,numberOfColumns,numberOfBombs);
+
   }
 
   get playerBoard() {
     return this._playerBoard;
   }
 
+  get bombBoard() {
+    return this._bombBoard;
+  }
+
+  get numberOfBombs() {
+    return this._numberOfBombs;
+  }
+
+  get numberOfTiles() {
+    return this._numberOfTiles;
+  };
+
   flipTile(rowIndex,columnIndex) {
     if (this._playerBoard[rowIndex][columnIndex] !== ' ') {
       console.log('This tile has already been flipped!');
-      return;
+      // return;
     } else if (this._bombBoard[rowIndex][columnIndex] === 'B') {
       this._playerBoard[rowIndex][columnIndex] = 'B';
     } else {
-      this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs[rowIndex,columnIndex];
+      //******* Issue: I had this._board.getNumberOfNeighborBombs when it should be this.getNumberOfNeighborBombs
+      this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
     };
     this._numberOfTitles--;
   }
 
-  getNumberOfNeighborBombs(rowIndex, columnIndex) {
+getNumberOfNeighborBombs(rowIndex, columnIndex){
     const neighborOffsets = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
     const numberOfRows = this._bombBoard.length;
     const numberOfColumns = this._bombBoard[0].length;
@@ -52,20 +70,20 @@ class Board {
       const neighborRowIndex = rowIndex + offset[0];
       const neighborColumnIndex = columnIndex + offset[1];
       if (neighborRowIndex >= 0 && neighborRowIndex <= numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex <= numberOfColumns) {
-        if (bombBoard[neighborRowIndex,neighborColumnIndex] === 'B') {
-          numberOfBombs++;
+        if (this._bombBoard[neighborRowIndex,neighborColumnIndex] === 'B') {
+          this._numberOfBombs++;
         }
       };
-      return numberOfBombs;
+
     });
   }
 
   hasSafeTiles() {
-    return this._numberOfTiles !== this._numberOfBombs;
+    return this._numberOfTiles === this._numberOfBombs;
   }
 
-  print(board) {
-    return this._board.map(row => row.join(' | ')).join('\n');
+  print() {
+    console.log(this._playerBoard.map(row => row.join(' | ')).join('\n'));
   }
 
   static generatePlayerBoard(numberOfRows, numberOfColumns) {
